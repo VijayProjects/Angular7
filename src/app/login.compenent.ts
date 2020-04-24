c12fb8b2a0f16f2633f3ffbb60a531c6f36b165b
vijay <vijaybabu.k007@gmail.com>
2020-04-24 15:06:20 +0530

    angular meterial added

--- src/app/login.compenent.ts
+++ src/app/login.compenent.ts
@@ -1,14 +1,14 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-  selector: "app-login",
+  selector: 'app-login',
   template: `
     <h2>Login</h2>
-    {{ msg }} <input type="text" size="40" /><br />
-    <input type="button" size="20" value="Submit" /><br />
+    {{ msg }} <input type='text' size='40' /><br />
+    <input type='button' size='20' value='Submit' /><br />
   `,
-  styles: ["h2{background-color:red;color:white;text-align:center}"]
+  styles: ['h2{background-color:red;color:white;text-align:center}']
 })
 export class LoginComponent {
-  msg = "Welcome to Angurar";
+  msg = 'Welcome to Angurar';
 }
